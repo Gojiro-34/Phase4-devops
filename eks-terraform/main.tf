87e38e9e506af6ec35d96268345cb62f1d7af064
Gojiro-34 <asifibrahim256@gmail.com>
2026-07-17 22:43:57 +0500

    workin on Aws eks , Vpc configurated for eks through terraform

---
+++ eks-terraform/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+	region= var.aws_region
+}
+
+module "vpc" {
+	source = "terraform-aws-modules/vpc/aws"
+	version= "~> 5.0"
+
+	name = "${var.project_name}-vpc"
+	cidr= "10.3.0.0/16"
+
+	azs= ["${var.aws_region}a" , "${var.aws_region}b"]
+	private_subnets = ["10.3.1.0/24" , "10.3.2.0/24"]
+	public_subnets = ["10.3.101.0/24" , "10.3.102.0/24"]
+
+	enable_nat_gateway= false
+	single_nat_gateway= false
+
+	public_subnet_tags= {
+		"kubernetes.io/role/elb" = "1"
+	}
+
+	private_subnet_tags= {
+		"kubernetes.io/role/internal-elb"= "1"
+	}
+}
